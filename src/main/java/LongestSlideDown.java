ae61cd19ae3331b53464dc4fb4f8777f0648f0a7
miguel.gonzalez <miguel.gonzalez@mango.com>
2017-06-24 00:43:39 +0200

    Refactor: for any row

--- src/main/java/LongestSlideDown.java
+++ src/main/java/LongestSlideDown.java
@@ -1,19 +1,10 @@
 public class LongestSlideDown {
     public static int longestSlideDown(int[][] test) {
-
-        if (test.length>3){
-            test[2][0] = test[2][0] + (test[3][0]>test[3][1]?test[3][0]:test[3][1]);
-            test[2][1] = test[2][1] + (test[3][1]>test[3][2]?test[3][1]:test[3][2]);
-            test[2][2] = test[2][2] + (test[3][2]>test[3][3]?test[3][2]:test[3][3]);
-
+        for (int i = test.length-1;i>1;i--){
+            for (int j = 0; j<i;j++){
+                test[i-1][j] = test[i-1][j] + (test[i][j]>test[i][j+1]?test[i][j]:test[i][j+1]);
+            }
         }
-
-        if (test.length>2){
-            test[1][0] = test[1][0] + (test[2][0]>test[2][1]?test[2][0]:test[2][1]);
-            test[1][1] = test[1][1] + (test[2][1]>test[2][2]?test[2][1]:test[2][2]);
-
-        }
-
         test[1][0] = test[1][0] + test[0][0];
         test[1][1] = test[1][1] + test[0][0];
 
