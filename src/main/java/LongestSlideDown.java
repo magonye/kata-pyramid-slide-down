42d69ec821d9e66675e2723c9000f678e32598f2
miguel.gonzalez <miguel.gonzalez@mango.com>
2017-06-24 00:54:09 +0200

    Refactor: level of abstraction

--- src/main/java/LongestSlideDown.java
+++ src/main/java/LongestSlideDown.java
@@ -1,14 +1,27 @@
 public class LongestSlideDown {
     public static int longestSlideDown(int[][] pyramid) {
+        calculateBiggerPathFromBottomToUp(pyramid);
+
+
+        return getBiggerElement(pyramid,1,0);
+    }
+
+    private static void calculateBiggerPathFromBottomToUp(int[][] pyramid) {
         for (int i = pyramid.length-1;i>1;i--){
-            for (int j = 0; j<i;j++){
-                pyramid[i-1][j] = pyramid[i-1][j] + getBiggerElement(pyramid,i,j);
-            }
+            calculateBiggerPath(pyramid, i);
+        }
+    }
+
+    private static void calculateBiggerPath(int[][] pyramid, int i) {
+        for (int j = 0; j<i;j++){
+            pyramid[i-1][j] = getBiggerStepPath(pyramid, i, j);
         }
         pyramid[1][0] = pyramid[1][0] + pyramid[0][0];
         pyramid[1][1] = pyramid[1][1] + pyramid[0][0];
+    }
 
-        return getBiggerElement(pyramid,1,0);
+    private static int getBiggerStepPath(int[][] pyramid, int i, int j) {
+        return pyramid[i-1][j] + getBiggerElement(pyramid,i,j);
     }
 
     private static int getBiggerElement(int[][] ints, int x, int y) {
