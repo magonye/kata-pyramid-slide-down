5a45b02e66146b7e6b5a53e7a1d9d315de98c877
miguel.gonzalez <miguel.gonzalez@mango.com>
2017-06-24 00:27:23 +0200

    Gree: 4 row test

--- src/main/java/LongestSlideDown.java
+++ src/main/java/LongestSlideDown.java
@@ -1,6 +1,13 @@
 public class LongestSlideDown {
     public static int longestSlideDown(int[][] test) {
 
+        if (test.length>3){
+            test[2][0] = test[2][0] + (test[3][0]>test[3][1]?test[3][0]:test[3][1]);
+            test[2][1] = test[2][1] + (test[3][1]>test[3][2]?test[3][1]:test[3][2]);
+            test[2][2] = test[2][2] + (test[3][2]>test[3][3]?test[3][2]:test[3][3]);
+
+        }
+
         if (test.length>2){
             test[1][0] = test[1][0] + (test[2][0]>test[2][1]?test[2][0]:test[2][1]);
             test[1][1] = test[1][1] + (test[2][1]>test[2][2]?test[2][1]:test[2][2]);
